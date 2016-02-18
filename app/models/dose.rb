3676b4bdfa7144b15b24bac39fc8b380ab6224f5
louis pons <louis.pons@icloud.com>
2016-02-18 13:13:22 +0100

    avant le routeur

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,9 @@
 class Dose < ActiveRecord::Base
+  validates :description, uniqueness: true, presence: true
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, presence: :true
-  validates :cocktail, presence: :true
-  validates :ingredient, presence: :true
 
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ActiveRecord::Base
-  has_many :doses, dependent: :destroy
+  has_many :doses
   # has_many :cocktails, through: :doses, dependent: :destroy
-  validates :doses, presence: :true
-  validates :name, uniqueness: :true
+  # validates :doses, presence: :true
+  validates :name, uniqueness: :true, presence: true
 end
